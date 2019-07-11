5ea540bf11914ebf93263c2a6b8d7da5c5e736d9
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-07-10 19:30:21 -0700

    basic start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
+
+import LandingPage from './components/LandingPage'
+import Header from './components/Header';
+import About from './components/Tabs/About';
+import Contact from './components/Tabs/Contact';
+
 
 
 class App extends Component {
@@ -7,7 +15,8 @@ class App extends Component {
     return (
       <Router>
         <div>
-
+          <Header />
+          <Route exact path="/" component={LandingPage} />
         </div>
       </Router>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
     render(){
         return (
-            <div>
-
+            <div style={mainContainer}>
+                This is Header
             </div>
         )
     };
 };
+
+const mainContainer = {
+    backgroundColor: '#333',
+    height: '10vh',
+    color: '#fff',
+    
+}
 
 export default Header;
